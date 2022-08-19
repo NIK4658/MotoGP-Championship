81784d7d6beb0bcfd43e9326d4cacb0b1017cfb3
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-08-19 11:26:36 +0200

    Create ApplicationStart.java

---
+++ src/start/ApplicationStart.java
@@ -0,0 +1,12 @@
+package start;
+
+import gui.MainGui;
+
+/**
+ * Starts the application.
+ */
+public final class ApplicationStart {
+    public static void main(final String[] args) {
+        new MainGui();
+    }
+}
